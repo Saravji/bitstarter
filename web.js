167607b24dbc3e050f09383cc403e92a288c9fa4
Saravji <dirk.biesinger@gmail.com>
2013-07-10 03:29:36 +0000

    web.js file write-out

--- web.js
+++ web.js
@@ -1,15 +1,16 @@
 var fs = require('fs');
 var express = require('express');
-
+var answer;
 var app = express.createServer(express.logger());
 
 fs.readFileSync('index.html', function (err, data) {
   if (err) throw err;
+  answer = data;
   console.log(data);
 }):
 
 app.get('/', function(request, response) {
-  response.send(data);
+  response.send(answer);
 });
 
 var port = process.env.PORT || 5000;
