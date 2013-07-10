b135d3ec709e8a2476599e859d87b11bd4b2bbf4
Saravji <dirk.biesinger@gmail.com>
2013-07-10 03:33:05 +0000

    web.js file write-out

--- web.js
+++ web.js
@@ -3,11 +3,11 @@ var express = require('express');
 var answer;
 var app = express.createServer(express.logger());
 
-fs.readFileSync('index.html', function (err, data) {
-  if (err) throw err;
-  answer = data;
-  console.log(data);
-}):
+//fs.readFileSync('index.html', function (err, data) {
+//  if (err) throw err;
+/ / answer = data;
+//  console.log(data);
+//}):
 
 app.get('/', function(request, response) {
   response.send('back to test');
